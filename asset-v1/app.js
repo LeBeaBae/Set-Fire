e38178e135088b50da516ddf9b3b855d5b7b9f3e
Thorley-Arya Öner <oner.thorley@gmail.com>
2023-08-08 13:56:38 -0500

    Update app.js

--- asset-v1/app.js
+++ asset-v1/app.js
@@ -1,127 +1,94 @@
 /*the creator of the make it a real mod not chinese or malaysia*/
 var app = new function() {
-  this.name = "Alpha",
-    this.version = "1",
-    this.date = "2009",
-    this.folder = "asset-v1/",
-    this.looptime = 13200,
-    this.bpm = 80,
-    this.totalframe = 260,
-    this.nbpolo = 7,
-    this.nbloopbonus = 12,
-    this.bonusloopA = !1,
-    this.bonusendloopA = !1,
-    this.recmaxloop = 34,
-    this.recminloop = 4,
-    this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop,
-    this.spritepolo = "polo-sprite.png",
-    this.spritepicto = "game-picto.png",
-    this.colBck = "#121212",
-    this.col0 = "#919191",
-    this.col1 = "#5f5f5f",
-    this.col2 = "#4b4b4b",
-    this.col3 = "#373737",
-    this.col4 = "#232323",
-    this.animearray = [
-      {
-          name: "1_lead",
-          color: "828282",
-          uniqsnd: !0
-      },
-      {
-          name: "2_deux",
-          color: "828282",
-          uniqsnd: !0
-      },
-      {
-          name: "3_kosh",
-          color: "828282",
-          uniqsnd: !0
-      },
-      {
-          name: "4_shpok",
-          color: "828282",
-          uniqsnd: !0
-      },
-      {
-          name: "5_tom",
-          color: "828282",
-          uniqsnd: !0
-      },
-      {
-          name: "6_nouana",
-          color: "828282",
-          uniqsnd: !0
-      },
-      {
-          name: "7_scratch",
-          color: "828282",
-          uniqsnd: !0
-      },
-      {
-          name: "8_trill",
-          color: "828282",
-          uniqsnd: !0
-      },
-      {
-          name: "9_bass",
-          color: "828282",
-          uniqsnd: !0
-      }, {
-          name: "10_uh",
-          color: "828282",
-          uniqsnd: !0
-      },
-      {
-          name: "11_nugu",
-          color: "828282",
-          uniqsnd: !0
-      }, {
-          name: "12_guit",
-          color: "828282",
-          uniqsnd: !0
-      }, {
-          name: "13_tromp",
-          color: "828282",
-          uniqsnd: !0
-      }, {
-          name: "14_pouin",
-          color: "828282",
-          uniqsnd: !0
-      }, {
-          name: "15_tung",
-          color: "828282",
-          uniqsnd: !0
-      }, {
-          name: "16_aoun",
-          color: "828282",
-          uniqsnd: !0
-      }, {
-          name: "17_hum",
-          color: "828282",
-          uniqsnd: !0
-      }, {
-          name: "18_get",
-          color: "828282",
-          uniqsnd: !0
-      }, {
-          name: "19_tellme",
-          color: "828282",
-          uniqsnd: !0
-      }, {
-          name: "20_make",
-          color: "828282",
-          uniqsnd: !0
-      }
-    ], this.bonusarray = [{
-        name: "The Choir",
-        src: "v1-b1-choir-hb.mp4",
-        code: "2,4,8,11,15",
-        sound: "bonus-choir",
+    this.name = "Set Fire", this.version = "1", this.date = "2009", this.folder = "asset-v1/", this.looptime = 6600, this.bpm = 80, this.totalframe = 260, this.nbpolo = 7, this.nbloopbonus = 4, this.bonusloopA = !2, this.bonusendloopA = !2, this.recmaxloop = 100, this.recminloop = 10, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#273C7F", this.col0 = "#273C7F", this.col1 = "#273C7F", this.col2 = "#273C7F", this.col3 = "#273C7F", this.col4 = "#273C7F", this.animearray = [{
+        name: "1_lead",
+        color: "EDCFAD",
+        uniqsnd: !1
+    }, {
+        name: "2_deux",
+        color: "FEDF25",
+        uniqsnd: !1
+    }, {
+        name: "3_kosh",
+        color: "CD0C2F",
+        uniqsnd: !0
+    }, {
+        name: "4_shpok",
+        color: "37046C",
+        uniqsnd: !1
+    }, {
+        name: "5_tom",
+        color: "23562B",
+        uniqsnd: !0
+    }, {
+        name: "6_nouana",
+        color: "FF0810",
+        uniqsnd: !1
+    }, {
+        name: "7_scratch",
+        color: "E4B5BB",
+        uniqsnd: !1
+    }, {
+        name: "8_trill",
+        color: "DA8BB3",
+        uniqsnd: !1
+    }, {
+        name: "9_bass",
+        color: "6ABE28",
+        uniqsnd: !1
+    }, {
+        name: "10_uh",
+        color: "676F96",
+        uniqsnd: !1
+    }, {
+        name: "11_nugu",
+        color: "786530",
+        uniqsnd: !1
+    }, {
+        name: "12_guit",
+        color: "FBF42B",
+        uniqsnd: !1
+    }, {
+        name: "13_tromp",
+        color: "FB060D",
+        uniqsnd: !0
+    }, {
+        name: "14_pouin",
+        color: "137232",
+        uniqsnd: !1
+    }, {
+        name: "15_tung",
+        color: "F73C05",
+        uniqsnd: !0
+    }, {
+        name: "16_aoun",
+        color: "E9F6FC",
+        uniqsnd: !1
+    }, {
+        name: "17_hum",
+        color: "0A82C0",
+        uniqsnd: !1
+    }, {
+        name: "18_get",
+        color: "21597E",
+        uniqsnd: !1
+    }, {
+        name: "19_tellme",
+        color: "4255A2",
+        uniqsnd: !1
+    }, {
+        name: "20_make",
+        color: "404C98",
+        uniqsnd: !1
+    }], this.bonusarray = [{
+        name: "Mad",
+        src: "v1-b1-mad-hb.mp4",
+        code: "1,2,11,14,16",
+        sound: "bonus-mad",
         aspire: "aspire"
     }];
-  for (var n = 0, o = this.animearray.length; n < o; n++) {
-      var a = this.animearray[n].name;
-      this.animearray[n].soundA = a + "_a", this.animearray[n].soundB = this.animearray[n].uniqsnd ? a + "_a" : a + "_b", this.animearray[n].anime = a + "-sprite.png", this.animearray[n].animeData = a + ".json";
-  }
+    for (var n = 0, o = this.animearray.length; n < o; n++) {
+        var a = this.animearray[n].name;
+        this.animearray[n].soundA = a + "_a", this.animearray[n].soundB = this.animearray[n].uniqsnd ? a + "_a" : a + "_b", this.animearray[n].anime = a + "-sprite.png", this.animearray[n].animeData = a + ".json";
+    }
 };
